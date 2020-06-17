9d5451e078f932812fde6b9c6276354a8c991a5d
StevePro7 <steveprozune@hotmail.com>
2020-06-17 15:58:02 +0100

    no message

---
+++ SGDK_Samples/sound/dev/src/main.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN__
+#define __MAIN__
+
+#ifdef _CONSOLE
+#include "_genesis.h"
+#pragma warning( disable : 4244 ) 
+#else
+#include <genesis.h>
+#endif
+
+#include "resources.h"
+
+#endif//__MAIN__
---
+++ SGDK_Samples/sound/sfx/res/resources.c
@@ -0,0 +1,32 @@
+#ifdef _CONSOLE
+
+#include "_genesis.h"
+
+const u8 sor2_xgm[78848] = { 0 };
+const u8 bapcm_xgm[128000] = { 0 };
+const u8 toystory[52224] = { 0 };
+const u8 midnight[106496] = { 0 };
+const u8 roadrash[73216] = { 0 };
+const u8 sonic1[21760] = { 0 };
+const u8 india_8k[166912] = { 0 };
+const u8 india_11k[229888] = { 0 };
+const u8 india_13k[279296] = { 0 };
+const u8 india_16k[333568] = { 0 };
+const u8 india_22k[459520] = { 0 };
+const u8 india_32k[666880] = { 0 };
+const u8 india_pcm_22k[229760] = { 0 };
+const u8 loop3_pcm_22k[107136] = { 0 };
+const u8 loop1_16k[66048] = { 0 };
+const u8 loop2_16k[66048] = { 0 };
+const u8 explode_16k[25600] = { 0 };
+const u8 hat1_16k[11008] = { 0 };
+const u8 hat2_16k[6400] = { 0 };
+const u8 snare1_16k[10752] = { 0 };
+const u8 snare2_16k[4352] = { 0 };
+const u8 explode_u8k[22528] = { 0 };
+const u8 loop1_14k[57856] = { 0 };
+const u8 hat1_14k[9728] = { 0 };
+const u8 snare1_14k[9472] = { 0 };
+const u8 cri_14k[8192] = { 0 };
+
+#endif

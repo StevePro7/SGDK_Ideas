6a090b65f4c19ed124e58bdc965f1ce15e019d37
StevePro7 <steveprozune@hotmail.com>
2020-06-17 15:47:15 +0100

    no message

---
+++ SGDK_Samples/astrofra/gfx/res/gfx.c
@@ -0,0 +1,8 @@
+#ifdef _CONSOLE
+
+#include "_genesis.h"
+
+const SpriteDefinition donut = { NULL, 0, NULL, 0, 0 };
+const Image starfield = { NULL, NULL, NULL };
+
+#endif
---
+++ SGDK_Samples/astrofra/lib/smp_null.h
@@ -0,0 +1,8 @@
+#include "types.h"
+
+#ifndef _SMP_NULL_H_
+#define _SMP_NULL_H_
+
+extern const u8 smp_null[0x100];
+
+#endif // _SMP_NULL_H_

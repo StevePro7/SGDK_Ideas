63b93d0a50bed40835c7fe7a9df640ee2466d994
StevePro7 <steveprozune@hotmail.com>
2020-06-17 15:54:45 +0100

    no message

---
+++ SGDK_Samples/joytest/lib/libres.h
@@ -0,0 +1,14 @@
+#include "bmp.h"
+#include "pal.h"
+#include "types.h"
+#include "vdp_tile.h"
+
+#ifndef _LIBRES_H_
+#define _LIBRES_H_
+
+extern const Bitmap logo_lib;
+extern const TileSet font_lib;
+extern const Palette font_pal_lib;
+extern const u8 stop_xgm[172];
+
+#endif//_LIBRES_H_

028822341004726e6c10720a5132db21b6714a50
StevePro7 <steveprozune@hotmail.com>
2020-10-05 20:50:54 +0100

    no message

--- SGDK_DemosX/24_ROM_Hack/dev/src/main.c
+++ SGDK_DemosX/24_ROM_Hack/dev/src/main.c
@@ -14,18 +14,8 @@ void updateScoreDisplay( u16 value, u16 x, u16 y )
 
 int main()
 {
-	s8 data;
-	s8 data2;
-
-	//engine_storage_manager_write();
-	data = engine_storage_manager_read();
-	data2 = engine_storage_manager_read2();
-
-	//data = 0;
-	//data2 = 1;
-	updateScoreDisplay( data, 2, 1 );
-	updateScoreDisplay( data2, 2, 2 );
-
+	u8 data = 0x40;
+	updateScoreDisplay( data, 10, 10 );
 	while( 1 )
 	{
 		VDP_waitVSync();

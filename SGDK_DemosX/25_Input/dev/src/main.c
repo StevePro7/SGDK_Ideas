f167e5e224a243457999b533c43de6be4a3dfebe
StevePro7 <steveprozune@hotmail.com>
2020-10-06 22:30:30 +0100

    no message

--- SGDK_DemosX/25_Input/dev/src/main.c
+++ SGDK_DemosX/25_Input/dev/src/main.c
@@ -1,22 +1,31 @@
 #include "main.h"
 
-
-
-
 int main()
 {
 	u16 data = 1;
-	engine_hack_manager_init();
-	engine_hack_manager_load();
-
-	//engine_font_manager_data( data, 8, 4 );
-	//engine_font_manager_data( 10, 8, 5 );
-	//engine_font_manager_data( 100, 8, 8 );
-
 	while( 1 )
 	{
 		data = JOY_readJoypad( 0 );
 		engine_font_manager_data( data, 8, 4 );
+
+		engine_input_manager_update();
+		if( engine_input_manager_move_buttonB() )
+		{
+			VDP_drawText( "BUTTON B", 10, 10 );
+		}
+		else if( engine_input_manager_move_buttonC() )
+		{
+			VDP_drawText( "BUTTON C", 10, 10 );
+		}
+		else
+		{
+			VDP_drawText( "         ", 10, 10 );
+		}
+		//if( engine_input_manager_hold_down() )
+		//{
+		//	VDP_drawText( "         ", 10, 10 );
+		//}
+
 		VDP_waitVSync();
 	}
 

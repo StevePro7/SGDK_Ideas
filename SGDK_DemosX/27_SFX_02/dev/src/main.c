79553778cc010ed0f1ec14b4be1f6a4cd866b4bf
StevePro7 <steveprozune@hotmail.com>
2020-10-15 20:40:25 +0100

    no message

--- SGDK_DemosX/27_SFX_02/dev/src/main.c
+++ SGDK_DemosX/27_SFX_02/dev/src/main.c
@@ -3,7 +3,7 @@
 int main()
 {
 	unsigned char input;
-	u8 audio;
+	const u8 *audio;
 	u8 index;
 
 	VDP_drawText( "START  : TEST", 5, 1 );
@@ -15,11 +15,11 @@ int main()
 	index = 0;
 	//audio = *myaudio[ index ];
 	//audio = sonic_kill_sfx;
-	//audio = sonic_dead_sfx;
+	audio = sonic_dead_sfx;
 	//audio = sonic_jump_sfx;
-	//SND_setPCM_XGM( SFX_DEAD, audio, sizeof( audio ) );
-	SND_setPCM_XGM( SFX_DEAD, sonic_dead_sfx, sizeof( sonic_dead_sfx ) );
-	SND_setPCM_XGM( SFX_KILL, sonic_kill_sfx, sizeof( sonic_kill_sfx ) );
+	SND_setPCM_XGM( SFX_DEAD, audio, sizeof( audio ) );
+	//SND_setPCM_XGM( SFX_DEAD, sonic_dead_sfx, sizeof( sonic_dead_sfx ) );
+	//SND_setPCM_XGM( SFX_KILL, sonic_kill_sfx, sizeof( sonic_kill_sfx ) );
 	//SND_setPCM_XGM( SFX_MIKE, sonic_mike_sfx, sizeof( sonic_mike_sfx ) );
 	//SND_setPCM_XGM( SFX_OVER, sonic_over_sfx, sizeof( sonic_over_sfx ) );
 	//SND_setPCM_XGM( SFX_RING, sonic_ring_sfx, sizeof( sonic_ring_sfx ) );

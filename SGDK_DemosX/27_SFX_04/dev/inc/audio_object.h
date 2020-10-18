7316417d8f676fc02653f4730a8c8b00c9f23a1d
StevePro7 <steveprozune@hotmail.com>
2020-10-18 19:39:46 +0100

    no message

---
+++ SGDK_DemosX/27_SFX_04/dev/inc/audio_object.h
@@ -0,0 +1,14 @@
+#ifndef _AUDIO_OBJECT_H_
+#define _AUDIO_OBJECT_H_
+
+#ifdef _CONSOLE
+#include "_genesis.h"
+#else
+#include <genesis.h>
+#endif
+
+//extern Image images;
+extern const u8 *myaudio[];
+extern const u32 myaudio_size[];
+
+#endif//_AUDIO_OBJECT_H_
---
+++ SGDK_DemosX/27_SFX_04/dev/res/sfx.h
@@ -0,0 +1,6 @@
+#ifndef _RES_SFX_H_
+#define _RES_SFX_H_
+
+extern const u8 van_halen_sfx[3465216];
+
+#endif // _RES_SFX_H_

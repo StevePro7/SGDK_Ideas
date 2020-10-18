53d553f6110f1fb7a9f6b5da60755cdc0c356293
StevePro7 <steveprozune@hotmail.com>
2020-10-18 19:50:50 +0100

    no message

---
+++ SGDK_DemosX/27_SFX_04/dev/res/gfx.h
@@ -0,0 +1,17 @@
+#ifndef _RES_GFX_H_
+#define _RES_GFX_H_
+
+extern const Image artCover;
+extern const Image artAbcde;
+extern const Image artBcdef;
+extern const Image artCdefg;
+extern const Image artDefgh;
+extern const Image artSteve;
+extern const Image artEfghi;
+extern const Image artFghij;
+extern const Image artGhijk;
+extern const Image artHijkl;
+extern const Image artIjklm;
+extern const Image artJklmn;
+
+#endif // _RES_GFX_H_
--- SGDK_DemosX/27_SFX_04/dev/src/font_manager.c
+++ SGDK_DemosX/27_SFX_04/dev/src/font_manager.c
@@ -6,7 +6,6 @@ char str_score[ MAX_CHAR ] = "0";
 // http://gendev.spritesmind.net/forum/viewtopic.php?t=3093
 void engine_font_manager_data( unsigned int value, u16 x, u16 y )
 {
-	u16 bank = SYS_getBank( 1 );
 	sprintf( str_score, "%8d", value );
 	VDP_clearText( x, y, MAX_CHAR );
 	VDP_drawText( str_score, x, y );

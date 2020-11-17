c47de0d690eba5845782b65d1825a2e837888d04
StevePro7 <steveprozune@hotmail.com>
2020-11-17 21:21:21 +0000

    no message

--- SGDK_DemosX/45_BubbleSort/dev/src/main.c
+++ SGDK_DemosX/45_BubbleSort/dev/src/main.c
@@ -1,35 +1,48 @@
 #include "main.h"
 
+// Reference
+// https://www.geeksforgeeks.org/bubble-sort
+//https://www.geeksforgeeks.org/search-insert-and-delete-in-a-sorted-array
 char str_score[ 3 ] = "0";
 
 void updateScoreDisplay(u16 value, u16 x, u16 y)
 {
 	//sprintf( str_score, "%d", score );
-	sprintf( str_score, "%d", value );
+	sprintf( str_score, "%3d", value );
 	VDP_clearText( x, y, 3 );
 	VDP_drawText( str_score, x, y );
 }
 
+int insertSorted( int arr[], int key, int capacity )
+{
+	// Cannot insert more elements if n is already 
+	// more than or equal to capcity 
+
+	int i;
+	for( i = capacity- 1; ( i >= 0 && arr[ i ] >= key ); i-- )
+		arr[ i + 1 ] = arr[ i ];
+
+	arr[ i + 1 ] = key;
+
+	return ( i + 1 );
+}
+
 int main()
 {
-	unsigned char score;
-	unsigned char maxim;
-	unsigned short percent;
+	int arr[ 6 ] = { 12, 16, 20, 40, 50, 70 };
+	int capacity = sizeof( arr ) / sizeof( arr[ 0 ] );
+	int n = 0;
+	int key = 69;
 
-	score = 7;
-	maxim = 8;
-	//if( 0 == maxim )
-	//{
-	//	percent = 0;
-	//}
-	//else
-	//{
-	//	percent = ( float ) score / ( float ) maxim * 100;
-	//}
+	//u16 arr[ 3 ] = { 60, 120, 180 };
+	//updateScoreDisplay( arr[ 0 ], 10, 10 );
+	//updateScoreDisplay( arr[ 1 ], 10, 11 );
+	//updateScoreDisplay( arr[ 2 ], 10, 12 );
 
-	percent = 0 == maxim ? 0 : ( float ) score / ( float ) maxim * 100;
+	n = insertSorted( arr, key, capacity );
+	//u16 capacity = 3;
+	//u16 key = 150;
 
-	updateScoreDisplay( percent, 10, 10 );
 	while( 1 )
 	{
 		VDP_waitVSync();

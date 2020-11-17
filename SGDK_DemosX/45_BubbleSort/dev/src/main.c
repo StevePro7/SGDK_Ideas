2ace846eec0373656a18da38706779c7893c4727
StevePro7 <steveprozune@hotmail.com>
2020-11-17 20:59:48 +0000

    no message

---
+++ SGDK_DemosX/45_BubbleSort/dev/src/main.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+char str_score[ 3 ] = "0";
+
+void updateScoreDisplay(u16 value, u16 x, u16 y)
+{
+	//sprintf( str_score, "%d", score );
+	sprintf( str_score, "%d", value );
+	VDP_clearText( x, y, 3 );
+	VDP_drawText( str_score, x, y );
+}
+
+int main()
+{
+	unsigned char score;
+	unsigned char maxim;
+	unsigned short percent;
+
+	score = 7;
+	maxim = 8;
+	//if( 0 == maxim )
+	//{
+	//	percent = 0;
+	//}
+	//else
+	//{
+	//	percent = ( float ) score / ( float ) maxim * 100;
+	//}
+
+	percent = 0 == maxim ? 0 : ( float ) score / ( float ) maxim * 100;
+
+	updateScoreDisplay( percent, 10, 10 );
+	while( 1 )
+	{
+		VDP_waitVSync();
+	}
+
+	return 0;
+}
+

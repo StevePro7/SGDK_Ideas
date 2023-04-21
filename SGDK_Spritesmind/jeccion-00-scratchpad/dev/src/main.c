a63caf1c88d39ad5821b393a72f97c4a8719e314
StevePro7 <steveprozune@hotmail.com>
2023-04-21 17:17:55 +0100

    no message

--- SGDK_Spritesmind/jeccion-00-scratchpad/dev/src/main.c
+++ SGDK_Spritesmind/jeccion-00-scratchpad/dev/src/main.c
@@ -1,65 +1,70 @@
 #include "main.h"
 
-static void handleInput();
+static void joyEvent(u16 joy, u16 changed, u16 state);
 
 int main()
 {
 	VDP_drawText( "Controles: FORMA SINCRONA", 6, 4 );
+	JOY_setEventHandler( joyEvent );
 	while( 1 )
 	{
-		handleInput();
 		VDP_waitVSync();
 	}
 
 	return 0;
 }
 
-static void handleInput()
+static void joyEvent( u16 joy, u16 changed, u16 state )
 {
-	//variable donde se guarda la entrada del mando
-	u16 value = JOY_readJoypad( JOY_1 );
-
 	//si pulsamos izquierda...
-	if( value & BUTTON_LEFT )    VDP_drawText( "IZQUIERDA", 8, 10 );
+	if( state & BUTTON_LEFT )    VDP_drawText( "IZQUIERDA", 8, 10 );
 
 	//si pulsamos derecha...
-	if( value & BUTTON_RIGHT )   VDP_drawText( "DERECHA", 20, 10 );
+	if( state & BUTTON_RIGHT )   VDP_drawText( "DERECHA", 20, 10 );
 
 	//si pulsamos izquierda...
-	if( value & BUTTON_UP )      VDP_drawText( "ARRIBA", 16, 8 );
+	if( state & BUTTON_UP )      VDP_drawText( "ARRIBA", 16, 8 );
 
 	//si pulsamos derecha...
-	if( value & BUTTON_DOWN )    VDP_drawText( "ABAJO", 16, 12 );
+	if( state & BUTTON_DOWN )    VDP_drawText( "ABAJO", 16, 12 );
 
 	//si pulsamos A...
-	if( value & BUTTON_A )       VDP_drawText( "BOTON A", 6, 18 );
+	if( state & BUTTON_A )       VDP_drawText( "BOTON A", 6, 18 );
 
 	//si pulsamos B...
-	if( value & BUTTON_B )       VDP_drawText( "BOTON B", 14, 18 );
+	if( state & BUTTON_B )       VDP_drawText( "BOTON B", 14, 18 );
 
 	//si pulsamos C...
-	if( value & BUTTON_C )       VDP_drawText( "BOTON C", 22, 18 );
+	if( state & BUTTON_C )       VDP_drawText( "BOTON C", 22, 18 );
 
 	//si pulsamos X...
-	if( value & BUTTON_X )       VDP_drawText( "BOTON X", 6, 20 );
+	if( state & BUTTON_X )       VDP_drawText( "BOTON X", 6, 20 );
 
 	//si pulsamos Y...
-	if( value & BUTTON_Y )       VDP_drawText( "BOTON Y", 14, 20 );
+	if( state & BUTTON_Y )       VDP_drawText( "BOTON Y", 14, 20 );
 
 	//si pulsamos Z...
-	if( value & BUTTON_Z )       VDP_drawText( "BOTON Z", 22, 20 );
+	if( state & BUTTON_Z )       VDP_drawText( "BOTON Z", 22, 20 );
 
 	//si pulsamos START...
-	if( value & BUTTON_START )   VDP_drawText( "START", 10, 16 );
+	if( state & BUTTON_START )   VDP_drawText( "START", 10, 16 );
 
 	//si pulsamos MODE...
-	if( value & BUTTON_MODE )    VDP_drawText( "MODE", 20, 16 );
+	if( state & BUTTON_MODE )    VDP_drawText( "MODE", 20, 16 );
 
 	//si NO pulsamos nada, borra todo del título hacia abajo (VDP_clearTextArea() trabaja en tiles)
-	if( ( !( value & BUTTON_UP ) ) && ( !( value & BUTTON_DOWN ) ) && ( !( value & BUTTON_LEFT ) ) &&
-		( !( value & BUTTON_RIGHT ) ) && ( !( value & BUTTON_A ) ) && ( !( value & BUTTON_B ) ) &&
-		( !( value & BUTTON_C ) ) && ( !( value & BUTTON_X ) ) && ( !( value & BUTTON_Y ) ) &&
-		( !( value & BUTTON_Z ) ) && ( !( value & BUTTON_MODE ) ) && ( !( value & BUTTON_START ) ) )
+	if( ( ( !( state & BUTTON_LEFT ) ) && ( changed & BUTTON_LEFT ) ) ||
+		( ( !( state & BUTTON_RIGHT ) ) && ( changed & BUTTON_RIGHT ) ) ||
+		( ( !( state & BUTTON_UP ) ) && ( changed & BUTTON_UP ) ) ||
+		( ( !( state & BUTTON_DOWN ) ) && ( changed & BUTTON_DOWN ) ) ||
+		( ( !( state & BUTTON_A ) ) && ( changed & BUTTON_A ) ) ||
+		( ( !( state & BUTTON_B ) ) && ( changed & BUTTON_B ) ) ||
+		( ( !( state & BUTTON_C ) ) && ( changed & BUTTON_C ) ) ||
+		( ( !( state & BUTTON_X ) ) && ( changed & BUTTON_X ) ) ||
+		( ( !( state & BUTTON_Y ) ) && ( changed & BUTTON_Y ) ) ||
+		( ( !( state & BUTTON_Z ) ) && ( changed & BUTTON_Z ) ) ||
+		( ( !( state & BUTTON_START ) ) && ( changed & BUTTON_START ) ) ||
+		( ( !( state & BUTTON_MODE ) ) && ( changed & BUTTON_MODE ) ) )
 	{
 		VDP_clearTextArea( 0, 7, 40, 23 );
 	}

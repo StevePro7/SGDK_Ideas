c902c2512b7796a7529ce9d2f6e104f17dbbb73f
StevePro7 <steveprozune@hotmail.com>
2023-04-22 19:06:31 +0100

    no message

---
+++ SGDK_Spritesmind/leccion 10-colisiones (4) - bullet hell mem dinamica optimiz/gfx/res/sprites.c
@@ -0,0 +1,9 @@
+#ifdef _CONSOLE
+
+#include "_genesis.h"
+
+const SpriteDefinition bala_sprite = { NULL, 0, NULL, 0, 0 
+const SpriteDefinition nave_sprite = { NULL, 0, NULL, 0, 0 
+const SpriteDefinition explosion_sprite = { NULL, 0, NULL, 0, 0 
+
+#endif

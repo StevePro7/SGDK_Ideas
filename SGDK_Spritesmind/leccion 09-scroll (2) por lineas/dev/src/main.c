0da74938f1a8b9c7c24307bca99e9b2cb3e73789
StevePro7 <steveprozune@hotmail.com>
2023-04-22 18:33:08 +0100

    no message

---
+++ SGDK_Spritesmind/leccion 09-scroll (2) por lineas/dev/src/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main()
+{
+	while( 1 )
+	{
+		VDP_waitVSync();
+	}
+
+	return 0;
+}
---
+++ SGDK_Spritesmind/leccion 09-scroll (2) por lineas/gfx/res/sprites.c
@@ -0,0 +1,7 @@
+#ifdef _CONSOLE
+
+#include "_genesis.h"
+
+const SpriteDefinition ryu_sprite = { NULL, 0, NULL, 0, 0 
+
+#endif

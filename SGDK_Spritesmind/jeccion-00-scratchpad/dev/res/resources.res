d02666f65b1517e2ddc0b7aa53ec4ad27cfd121f
StevePro7 <steveprozune@hotmail.com>
2023-04-22 16:42:48 +0100

    no message

--- SGDK_Spritesmind/jeccion-00-scratchpad/dev/res/resources.res
+++
@@ -1,3 +0,0 @@
-IMAGE block "moon.png" 0
-
-IMAGE block2 "my64tiles.png" 0
---
+++ SGDK_Spritesmind/leccion 05-sprites (1)/gfx/res/sprites.c
@@ -0,0 +1,8 @@
+#ifdef _CONSOLE
+
+#include "_genesis.h"
+
+const Image block = { NULL, NULL, NULL };
+const Image block2 = { NULL, NULL, NULL };
+
+#endif

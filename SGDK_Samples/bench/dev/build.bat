e06d482f7d6f25aafec743685570763c0d517755
StevePro7 <steveprozune@hotmail.com>
2023-04-27 22:18:39 +0100

    no message

--- SGDK_Samples/bench/dev/build.bat
+++ SGDK_Samples/bench/dev/build.bat
@@ -11,8 +11,8 @@ set /a _started=_hours*60*60*100+_min*60*100+_sec*100+_cs
 
 
 :: Build
-::%GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen
-%GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen > NUL
+%GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen
+::%GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen > NUL
 
 
 :: Time build -END-

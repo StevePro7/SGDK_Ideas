136ac985af1f0c63bf9ca8cf0190381bf5b0c717
StevePro7 <steveprozune@hotmail.com>
2025-01-09 19:57:31 +0000

    no message

--- SGDK_MDavdnetures/Lesson11/dev/res/fondos.res
+++ SGDK_MDavdnetures/Lesson11/dev/res/fondos.res
@@ -1,3 +1,2 @@
-IMAGE fondo1 "fondos/fondo1.png" BEST
-
-
+IMAGE fondo01_01  		"fondos/fase01_01.png"  		BEST
+IMAGE fondo01_01_torres 	"fondos/fase01_01_torres.png" 		BEST
--- SGDK_MDavdnetures/Lesson13/dev/res/gfx.h
+++ SGDK_MDavdnetures/Lesson13/dev/res/gfx.h
@@ -3,5 +3,7 @@
 
 extern const Image bgb_image;
 extern const Image bga_image;
+extern const Image horizontal_hud;
+extern const Image vertical_hud;
 
 #endif // _RES_GFX_H_

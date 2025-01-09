1bb6381b56b167b3598c9fa32d2b13c41a840bfd
StevePro7 <steveprozune@hotmail.com>
2025-01-09 20:11:29 +0000

    no message

--- SGDK_MDavdnetures/Lesson08b/dev/res/sprite.h
+++ SGDK_MDavdnetures/Lesson08b/dev/res/sprite.h
@@ -2,6 +2,6 @@
 #define _RES_SPRITE_H_
 
 extern const SpriteDefinition sonic_sprite;
-extern const SpriteDefinition barra_sprite;
+extern const SpriteDefinition circulo_sprite;
 
 #endif // _RES_SPRITE_H_

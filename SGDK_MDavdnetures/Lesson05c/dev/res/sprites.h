7c5e8b04f645e650eb5682570da921ab1b3e33ce
StevePro7 <steveprozune@hotmail.com>
2025-01-10 12:17:39 +0000

    no message

--- SGDK_MDavdnetures/Lesson05c/dev/res/sprites.h
+++ SGDK_MDavdnetures/Lesson05c/dev/res/sprites.h
@@ -1,6 +1,12 @@
 #ifndef _RES_SPRITES_H_
 #define _RES_SPRITES_H_
 
+#ifdef _CONSOLE
+#include "_genesis.h"
+#else
+#include <genesis.h>
+#endif
+
 extern const SpriteDefinition sonic_sprite;
 extern const SpriteDefinition enemies_sprite;
 
---
+++ SGDK_MDavdnetures/Lesson05c/gfx/res/sprites.c
@@ -0,0 +1,4 @@
+#include "sprites.h"
+
+const SpriteDefinition sonic_sprite = { NULL, 0, NULL, 0, 0 };
+const SpriteDefinition enemies_sprite = { NULL, 0, NULL, 0, 0 };

247203693abaebcdb4d7f3bb3f51971ea2dbd08e
StevePro7 <steveprozune@hotmail.com>
2025-01-10 17:57:59 +0000

    no message

--- SGDK_MDavdnetures/Lesson08b/dev/res/fondos.h
+++ SGDK_MDavdnetures/Lesson08b/dev/res/fondos.h
@@ -1,6 +1,12 @@
 #ifndef _RES_FONDOS_H_
 #define _RES_FONDOS_H_
 
+#ifdef _CONSOLE
+#include "_genesis.h"
+#else
+#include <genesis.h>
+#endif
+
 extern const Image fondo1;
 extern const Image fondo2;
 
--- SGDK_MDavdnetures/Lesson08c/dev/res/sprite.h
+++ SGDK_MDavdnetures/Lesson08c/dev/res/sprite.h
@@ -1,6 +1,12 @@
 #ifndef _RES_SPRITE_H_
 #define _RES_SPRITE_H_
 
+#ifdef _CONSOLE
+#include "_genesis.h"
+#else
+#include <genesis.h>
+#endif
+
 extern const SpriteDefinition mi_sprite_ryu;
 extern const SpriteDefinition mi_sprite_sombra;
 extern const SpriteDefinition mi_sprite_hadoken;
